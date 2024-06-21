0b2c17e6aee32a0360b9351267c85c4cb85fa927
Gustavo Victor <gguvictor909@gmail.com>
2024-06-21 20:31:51 -0300

    feat: update project config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // modules
 import express from "express";
 import { config } from "dotenv";
-import path from "node:path";
+import path, { dirname } from "node:path";
 import url from "node:url";
 import cors from "cors";
 import Router from "./routes/Router.js";
@@ -26,6 +26,7 @@ const frontEndHost = "https://reactgram-alpha.vercel.app/";
 
 // dirname
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const baseDir = `${__dirname}/client/dist/`; 
 
 
 // middlewares
@@ -34,7 +35,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, "client/dist")));
 app.use("/", Router);
+
+
+app.get("*", (req, res) => {
+    res.sendFile("index.html", { root: baseDir }); 
+});
 
 
 // port config 
