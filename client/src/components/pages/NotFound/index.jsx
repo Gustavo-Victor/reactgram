49a52f80ae18526fe9597dc0fa9e32a19ff1ab95
Gustavo Victor <gguvictor909@gmail.com>
2024-06-21 22:25:30 -0300

    feat: update not found page

--- client/src/components/pages/NotFound/index.jsx
+++ client/src/components/pages/NotFound/index.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import "./style.css"; 
+
+
 export default function NotFound() {
     return (
-        <div>
-            <h2>Not Found</h2>
+        <div id="not-found">
+            <h2>Page not found 😟</h2>
+            <p>The page you are looking for does not exist.</p>
+            <Link to="/" className="btn">Go Back</Link>
         </div>
     )
 }
